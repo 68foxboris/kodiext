9e2bf013dbf0b463eaa6d32dfcc0393d16203342
Boris <boris.fox.solovyev@gmail.com>
2023-10-18 22:13:28 +0300

    Fix ImportError: cannot import name 'video_hw' from 'Plugins.SystemPlugins.Videomode.VideoHardware'

--- src/plugin/plugin.py
+++ src/plugin/plugin.py
@@ -24,7 +24,7 @@ from Tools.Directories import fileExists, isPluginInstalled
 from Tools import Notifications
 
 from Components.config import config
-from Plugins.SystemPlugins.Videomode.VideoHardware import video_hw
+from Plugins.SystemPlugins.Videomode.VideoHardware import VIDEO
 
 from .e2utils import InfoBarAspectChange, WebPixmap, MyAudioSelection, \
     StatusScreen, getPlayPositionInSeconds, getDurationInSeconds, \
@@ -169,7 +169,7 @@ class SetResolution:
             for res in resolutions:
                 for rate in rates:
                     try:
-                        if video_hw.isModeAvailable(self.port, res, rate):
+                        if VIDEO.isModeAvailable(self.port, res, rate):
                             self.kodires = res
                             self.kodirate = rate
                     except:
@@ -178,11 +178,11 @@ class SetResolution:
     def switch(self,Tokodi=False, Player=False):
         if Tokodi:
             if self.kodires and self.kodirate and self.port:
-                video_hw.setMode(self.port, self.kodires, self.kodirate)
+                VIDEO.setMode(self.port, self.kodires, self.kodirate)
                 open("/proc/stb/video/videomode", "w").write(self.kodires+self.kodirate.replace("Hz", ""))
         else:
             if self.E2res and self.rate and self.port:
-                video_hw.setMode(self.port, self.E2res, self.rate)
+                VIDEO.setMode(self.port, self.E2res, self.rate)
 
     def ReadData(self):
         self.E2res = config.av.videomode[self.port].value
